327d51dae3c21599e2c2553924da112ef6c62168
Anil S Keshavamurthy <anilsk@gmail.com>
2017-05-13 09:52:26 -0700

    Vim setup file

--- vim-setup.sh
+++ vim-setup.sh
@@ -5,9 +5,12 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -so ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/bundle
 git clone git://github.com/Lokaltog/vim-powerline.git
-cd ~/.vim/bundle 
-git clone https://github.com/kien/ctrlp.vim.git 
+cd ~/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
 cd ~/.vim/bundle
 git clone git://github.com/davidhalter/jedi-vim.git
 mkdir -p ~/.vim/ftplugin
 wget -O ~/.vim/ftplugin/python_editing.vim http://www.vim.org/scripts/download_script.php?src_id=5492
+
+#NOTE - manually install jedi for python
+#sudo pip install jedi
